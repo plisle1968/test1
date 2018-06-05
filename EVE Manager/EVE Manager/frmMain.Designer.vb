b79fbbef7ccdb6f16858658623bfb131060aa27e
plisle1968 <39959426+plisle1968@users.noreply.github.com>
2018-06-05 16:05:52 +0100

    Add files via upload

---
+++ EVE Manager/EVE Manager/frmMain.Designer.vb
@@ -0,0 +1,282 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim TreeNode2 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Node0")
+        Me.tcMain = New System.Windows.Forms.TabControl
+        Me.tpItemDetails = New System.Windows.Forms.TabPage
+        Me.lblTotalVal = New System.Windows.Forms.Label
+        Me.lblTotalValLabel = New System.Windows.Forms.Label
+        Me.lvMats = New System.Windows.Forms.ListView
+        Me.ColumnHeader3 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader4 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader5 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader6 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader7 = New System.Windows.Forms.ColumnHeader
+        Me.lvTypes = New System.Windows.Forms.ListView
+        Me.ColumnHeader1 = New System.Windows.Forms.ColumnHeader
+        Me.ColumnHeader2 = New System.Windows.Forms.ColumnHeader
+        Me.txtTypeDescription = New System.Windows.Forms.TextBox
+        Me.txtGroupDescription = New System.Windows.Forms.TextBox
+        Me.tvGroups = New System.Windows.Forms.TreeView
+        Me.TabPage2 = New System.Windows.Forms.TabPage
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.msMain = New System.Windows.Forms.MenuStrip
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.DataToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.LoadCurrentPricesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.tcMain.SuspendLayout()
+        Me.tpItemDetails.SuspendLayout()
+        Me.msMain.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tcMain
+        '
+        Me.tcMain.Controls.Add(Me.tpItemDetails)
+        Me.tcMain.Controls.Add(Me.TabPage2)
+        Me.tcMain.Location = New System.Drawing.Point(12, 52)
+        Me.tcMain.Name = "tcMain"
+        Me.tcMain.SelectedIndex = 0
+        Me.tcMain.Size = New System.Drawing.Size(1015, 623)
+        Me.tcMain.TabIndex = 0
+        '
+        'tpItemDetails
+        '
+        Me.tpItemDetails.Controls.Add(Me.lblTotalVal)
+        Me.tpItemDetails.Controls.Add(Me.lblTotalValLabel)
+        Me.tpItemDetails.Controls.Add(Me.lvMats)
+        Me.tpItemDetails.Controls.Add(Me.lvTypes)
+        Me.tpItemDetails.Controls.Add(Me.txtTypeDescription)
+        Me.tpItemDetails.Controls.Add(Me.txtGroupDescription)
+        Me.tpItemDetails.Controls.Add(Me.tvGroups)
+        Me.tpItemDetails.Location = New System.Drawing.Point(4, 22)
+        Me.tpItemDetails.Name = "tpItemDetails"
+        Me.tpItemDetails.Padding = New System.Windows.Forms.Padding(3)
+        Me.tpItemDetails.Size = New System.Drawing.Size(1007, 597)
+        Me.tpItemDetails.TabIndex = 0
+        Me.tpItemDetails.Text = "Item Details"
+        Me.tpItemDetails.UseVisualStyleBackColor = True
+        '
+        'lblTotalVal
+        '
+        Me.lblTotalVal.AutoSize = True
+        Me.lblTotalVal.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalVal.Location = New System.Drawing.Point(760, 484)
+        Me.lblTotalVal.MinimumSize = New System.Drawing.Size(200, 0)
+        Me.lblTotalVal.Name = "lblTotalVal"
+        Me.lblTotalVal.Size = New System.Drawing.Size(200, 18)
+        Me.lblTotalVal.TabIndex = 29
+        Me.lblTotalVal.Text = "0.00"
+        Me.lblTotalVal.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblTotalValLabel
+        '
+        Me.lblTotalValLabel.AutoSize = True
+        Me.lblTotalValLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalValLabel.Location = New System.Drawing.Point(569, 484)
+        Me.lblTotalValLabel.Name = "lblTotalValLabel"
+        Me.lblTotalValLabel.Size = New System.Drawing.Size(97, 18)
+        Me.lblTotalValLabel.TabIndex = 28
+        Me.lblTotalValLabel.Text = "Total Value:"
+        '
+        'lvMats
+        '
+        Me.lvMats.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader5, Me.ColumnHeader6, Me.ColumnHeader7})
+        Me.lvMats.FullRowSelect = True
+        Me.lvMats.Location = New System.Drawing.Point(558, 6)
+        Me.lvMats.MultiSelect = False
+        Me.lvMats.Name = "lvMats"
+        Me.lvMats.Size = New System.Drawing.Size(443, 457)
+        Me.lvMats.TabIndex = 27
+        Me.lvMats.UseCompatibleStateImageBehavior = False
+        Me.lvMats.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "ID"
+        Me.ColumnHeader3.Width = 0
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Type (Item)"
+        Me.ColumnHeader4.Width = 125
+        '
+        'ColumnHeader5
+        '
+        Me.ColumnHeader5.Text = "Qty"
+        Me.ColumnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.ColumnHeader5.Width = 80
+        '
+        'ColumnHeader6
+        '
+        Me.ColumnHeader6.Text = "Cost ea(ISK)"
+        Me.ColumnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.ColumnHeader6.Width = 80
+        '
+        'ColumnHeader7
+        '
+        Me.ColumnHeader7.Text = "Total (ISK)"
+        Me.ColumnHeader7.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.ColumnHeader7.Width = 100
+        '
+        'lvTypes
+        '
+        Me.lvTypes.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2})
+        Me.lvTypes.FullRowSelect = True
+        Me.lvTypes.Location = New System.Drawing.Point(282, 6)
+        Me.lvTypes.MultiSelect = False
+        Me.lvTypes.Name = "lvTypes"
+        Me.lvTypes.Size = New System.Drawing.Size(270, 496)
+        Me.lvTypes.TabIndex = 26
+        Me.lvTypes.UseCompatibleStateImageBehavior = False
+        Me.lvTypes.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "ID"
+        Me.ColumnHeader1.Width = 0
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Type (Item)"
+        Me.ColumnHeader2.Width = 245
+        '
+        'txtTypeDescription
+        '
+        Me.txtTypeDescription.Enabled = False
+        Me.txtTypeDescription.Location = New System.Drawing.Point(282, 508)
+        Me.txtTypeDescription.Multiline = True
+        Me.txtTypeDescription.Name = "txtTypeDescription"
+        Me.txtTypeDescription.Size = New System.Drawing.Size(270, 123)
+        Me.txtTypeDescription.TabIndex = 3
+        '
+        'txtGroupDescription
+        '
+        Me.txtGroupDescription.Enabled = False
+        Me.txtGroupDescription.Location = New System.Drawing.Point(6, 508)
+        Me.txtGroupDescription.Multiline = True
+        Me.txtGroupDescription.Name = "txtGroupDescription"
+        Me.txtGroupDescription.Size = New System.Drawing.Size(270, 123)
+        Me.txtGroupDescription.TabIndex = 1
+        '
+        'tvGroups
+        '
+        Me.tvGroups.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.tvGroups.Location = New System.Drawing.Point(6, 6)
+        Me.tvGroups.Name = "tvGroups"
+        TreeNode2.Name = "Node0"
+        TreeNode2.Text = "Node0"
+        Me.tvGroups.Nodes.AddRange(New System.Windows.Forms.TreeNode() {TreeNode2})
+        Me.tvGroups.Size = New System.Drawing.Size(270, 496)
+        Me.tvGroups.TabIndex = 0
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(1007, 637)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "TabPage2"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'msMain
+        '
+        Me.msMain.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.DataToolStripMenuItem})
+        Me.msMain.Location = New System.Drawing.Point(0, 0)
+        Me.msMain.Name = "msMain"
+        Me.msMain.Size = New System.Drawing.Size(1039, 24)
+        Me.msMain.TabIndex = 1
+        Me.msMain.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'DataToolStripMenuItem
+        '
+        Me.DataToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LoadCurrentPricesToolStripMenuItem})
+        Me.DataToolStripMenuItem.Name = "DataToolStripMenuItem"
+        Me.DataToolStripMenuItem.Size = New System.Drawing.Size(43, 20)
+        Me.DataToolStripMenuItem.Text = "Data"
+        '
+        'LoadCurrentPricesToolStripMenuItem
+        '
+        Me.LoadCurrentPricesToolStripMenuItem.Name = "LoadCurrentPricesToolStripMenuItem"
+        Me.LoadCurrentPricesToolStripMenuItem.Size = New System.Drawing.Size(177, 22)
+        Me.LoadCurrentPricesToolStripMenuItem.Text = "Load Current Prices"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1039, 687)
+        Me.Controls.Add(Me.tcMain)
+        Me.Controls.Add(Me.msMain)
+        Me.Name = "frmMain"
+        Me.Text = "Form1"
+        Me.tcMain.ResumeLayout(False)
+        Me.tpItemDetails.ResumeLayout(False)
+        Me.tpItemDetails.PerformLayout()
+        Me.msMain.ResumeLayout(False)
+        Me.msMain.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents tcMain As System.Windows.Forms.TabControl
+    Friend WithEvents tpItemDetails As System.Windows.Forms.TabPage
+    Friend WithEvents tvGroups As System.Windows.Forms.TreeView
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
+    Friend WithEvents txtGroupDescription As System.Windows.Forms.TextBox
+    Friend WithEvents txtTypeDescription As System.Windows.Forms.TextBox
+    Friend WithEvents lvTypes As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents lvMats As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader5 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader6 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader7 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents lblTotalValLabel As System.Windows.Forms.Label
+    Friend WithEvents lblTotalVal As System.Windows.Forms.Label
+    Friend WithEvents msMain As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents DataToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LoadCurrentPricesToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ EVE Manager/EVE Manager/frmMain.vb
@@ -0,0 +1,259 @@
+﻿Imports System.Data.SqlClient
+Imports System.IO
+Imports System.Reflection
+Imports System.Text
+Imports System.Web
+Imports System.Net
+Imports System.Xml
+
+Public Class frmMain
+
+#Region "Variable Definitions"
+    Private connStr As String = "Data Source=PETER-NEW-HOME\SQLEXPRESS;Initial Catalog=EveStaticData;Integrated Security=True;"
+    Private tpsURL As String = "http://api.eve-central.com/api/marketstat"
+    Private dbEve As New DataSet
+#End Region
+
+#Region "Form - functions"
+
+    Private Sub frmMain_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        'Load internal tables
+        LoadData()
+
+    End Sub
+
+    Private Sub LoadData()
+
+        Dim sqlStr As String
+        Dim sqlComm As New SqlCommand
+        Dim tempDataAdapter = New SqlDataAdapter()
+        Dim sqlConn As New SqlConnection(connStr)
+
+        If Not dbEve.Tables("Groups") Is Nothing Then
+            dbEve.Tables("Groups").Constraints.Clear()
+            dbEve.Tables("Groups").Clear()
+        End If
+
+        If Not dbEve.Tables("Types") Is Nothing Then
+            dbEve.Tables("Types").Constraints.Clear()
+            dbEve.Tables("Types").Clear()
+        End If
+
+        If Not dbEve.Tables("Mats") Is Nothing Then
+            dbEve.Tables("Mats").Constraints.Clear()
+            dbEve.Tables("Mats").Clear()
+        End If
+
+        sqlStr = "SELECT * FROM invMarketGroups"
+        sqlConn.Open()
+        sqlComm.CommandText = sqlStr
+        sqlComm.Connection = sqlConn
+        tempDataAdapter.SelectCommand = sqlComm
+        tempDataAdapter.Fill(dbEve, "Groups")
+        sqlConn.Close()
+
+        sqlStr = "SELECT * FROM invTypes"
+        sqlConn.Open()
+        sqlComm.CommandText = sqlStr
+        sqlComm.Connection = sqlConn
+        tempDataAdapter.SelectCommand = sqlComm
+        tempDataAdapter.Fill(dbEve, "Types")
+        sqlConn.Close()
+
+        sqlStr = "SELECT * FROM invTypeMaterials"
+        sqlConn.Open()
+        sqlComm.CommandText = sqlStr
+        sqlComm.Connection = sqlConn
+        tempDataAdapter.SelectCommand = sqlComm
+        tempDataAdapter.Fill(dbEve, "Mats")
+        sqlConn.Close()
+
+        Me.tcMain.SelectedIndex = 1
+        Me.tcMain.SelectedIndex = 0
+
+    End Sub
+
+    Private Sub tcMain_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles tcMain.SelectedIndexChanged
+
+        Select Case tcMain.SelectedIndex
+            Case Is = 0 'Item details page
+                PopulateGroupRootNodes()
+
+        End Select
+
+    End Sub
+
+    Private Sub LoadCurrentPricesToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LoadCurrentPricesToolStripMenuItem.Click
+
+        Dim pricesToGet As DataRow()
+        Dim post As StringBuilder
+        Dim webReq As HttpWebRequest
+        Dim webReader As StreamReader
+        Dim xmlDoc As XmlDocument
+        Dim xmlBuy, xmlSell As XmlNode
+
+        Dim webResult As String
+
+        pricesToGet = dbEve.Tables("Types").Select("getPrices = 1")
+
+        For Each priceToGet As DataRow In pricesToGet
+
+            Try
+                post = New StringBuilder
+
+                With post
+                    .AppendFormat("?cachebuster={0}", HttpUtility.UrlEncode("1"))
+                    .AppendFormat("&regionlimit={0}", HttpUtility.UrlEncode("10000068"))
+                    .AppendFormat("&typeid={0}", HttpUtility.UrlEncode(priceToGet("typeID")))
+                End With
+
+                webReq = CType(WebRequest.Create(tpsURL & post.ToString), HttpWebRequest)
+                webReq.Method = "GET"
+
+                webReader = New StreamReader(webReq.GetResponse().GetResponseStream())
+                webResult = webReader.ReadToEnd
+                webReader.Close()
+
+                xmlDoc = New XmlDocument
+                xmlDoc.LoadXml(webResult)
+
+                xmlBuy = xmlDoc.SelectSingleNode("/evec_api/marketstat/type/buy")
+                xmlSell = xmlDoc.SelectSingleNode("/evec_api/marketstat/type/sell")
+
+                priceToGet("buy") = CDbl(xmlBuy.Item("max").InnerText)
+                priceToGet("sell") = CDbl(xmlSell.Item("min").InnerText)
+            Catch ex As Exception
+                MsgBox(ex.Message.ToString)
+            End Try
+
+        Next
+
+    End Sub
+
+#End Region
+
+#Region "Item details page - functions"
+
+    Private Sub PopulateGroupRootNodes()
+
+        Me.tvGroups.Nodes.Clear()
+        Dim rowList As DataRow()
+        Dim tvNode As TreeNode
+
+        rowList = GetGroups("IS NULL")
+
+        For Each row As DataRow In rowList
+            tvNode = New TreeNode
+            tvNode.Text = row("marketGroupName")
+            tvNode.Tag = row("marketGroupID")
+
+            If Not IsDBNull(row("description")) Then
+                tvNode.ToolTipText = row("description")
+            End If
+
+            PopulateGroupChildNodes("= " & row("marketGroupID"), tvNode)
+            Me.tvGroups.Nodes.Add(tvNode)
+        Next
+
+    End Sub
+
+    Private Sub PopulateGroupChildNodes(ByVal parent As String, ByVal parentNode As TreeNode)
+
+        Dim rowList As DataRow()
+        Dim tvNode As TreeNode
+
+        rowList = GetGroups(parent)
+
+        For Each row As DataRow In rowList
+            tvNode = New TreeNode
+            tvNode.Text = row("marketGroupName")
+            tvNode.Tag = row("marketGroupID")
+
+            If Not IsDBNull(row("description")) Then
+                tvNode.ToolTipText = row("description")
+            End If
+
+            PopulateGroupChildNodes("= " & row("marketGroupID"), tvNode)
+            parentNode.Nodes.Add(tvNode)
+        Next
+
+    End Sub
+
+    Private Function GetGroups(ByVal Parent As String) As DataRow()
+
+        GetGroups = dbEve.Tables("Groups").Select("parentGroupID " & Parent, "marketGroupName")
+
+    End Function
+
+    Private Sub tvGroups_NodeMouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.TreeNodeMouseClickEventArgs) Handles tvGroups.NodeMouseClick
+
+        Dim tempNode As TreeNode = DirectCast(e.Node, TreeNode)
+        Dim typeList As DataRow()
+        Dim typeItem As ListViewItem
+
+        Me.txtGroupDescription.Text = tempNode.ToolTipText
+        typeList = dbEve.Tables("Types").Select("marketGroupID = " & tempNode.Tag, "typeName")
+        Me.lvTypes.Items.Clear()
+
+        For Each typeListItem As DataRow In typeList
+            typeItem = New ListViewItem
+            typeItem.Text = typeListItem("typeID")
+            typeItem.SubItems.Add(typeListItem("typeName"))
+
+            If Not IsDBNull(typeListItem("description")) Then
+                typeItem.Tag = typeListItem("description")
+            End If
+
+            Me.lvTypes.Items.Add(typeItem)
+        Next
+
+    End Sub
+
+    Private Sub lvTypes_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles lvTypes.SelectedIndexChanged
+
+        If lvTypes.FocusedItem Is Nothing Then Exit Sub
+        Me.txtTypeDescription.Text = lvTypes.Items(lvTypes.FocusedItem.Index).Tag
+        PopulateMats(lvTypes.Items(lvTypes.FocusedItem.Index).SubItems(0).Text)
+
+    End Sub
+
+    Private Sub PopulateMats(ByVal typeID As String)
+
+        Me.lvMats.Items.Clear()
+
+        Dim matsList, matDetails As DataRow()
+        Dim matsItem As ListViewItem
+        Dim matVal, totVal As Single
+        Dim matQty As Integer
+
+        totVal = 0
+        matsList = dbEve.Tables("Mats").Select("typeID = " & typeID, "materialTypeID")
+
+        For Each mat As DataRow In matsList
+            matDetails = dbEve.Tables("Types").Select("typeID = " & mat("materialTypeID"))
+            matsItem = New ListViewItem
+            matsItem.Text = matDetails(0)("typeID")
+            matsItem.SubItems.Add(matDetails(0)("typeName"))
+            matsItem.SubItems.Add(Format(mat("quantity"), "#,##0"))
+
+            If IsDBNull(matDetails(0)("buy")) Then
+                matsItem.SubItems.Add("0.00")
+                matsItem.SubItems.Add("0.00")
+            Else
+                matVal = CSng(matDetails(0)("buy"))
+                matQty = CInt(mat("quantity"))
+                matsItem.SubItems.Add(Format(matVal, "#,##0.00"))
+                matsItem.SubItems.Add(Format(matVal * matQty, "#,##0.00"))
+                totVal += (matVal * matQty)
+            End If
+
+            Me.lvMats.Items.Add(matsItem)
+        Next
+
+        Me.lblTotalVal.Text = Format(totVal, "#,##0.00")
+    End Sub
+
+#End Region
+
+End Class
